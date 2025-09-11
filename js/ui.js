bc0ce8b7317ec600d0627afaa0d2126a8cb8a786
davidgarob <davidgarob@gmail.com>
2025-09-11 10:27:20 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,4 @@
+import { simularPartido, DEFAULT_EVENTS } from './engine.js';
+const resultado = simularPartido(equipoT, equipoR, { N: 12, eventsDeck: DEFAULT_EVENTS });
+console.log(resultado.log.join('\n'), resultado.score);
 
